672b43b98af6dd8e1806728d70b3390d76218ae6
Shreya Subramanian <ss2745@cornell.edu>
2020-04-22 20:27:47 -0400

    Code backup for search controller

--- app/datafiles/tokenizereviews.py
+++ app/datafiles/tokenizereviews.py
@@ -1,12 +1,51 @@
+from . import *
+from app.irsystem.models.helpers import *
+from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 import csv 
 import re
 import nltk 
 from nltk.corpus import stopwords
-# nltk.download('stopwords')
-# nltk.download('punkt')
+from pathlib import Path
+import pathlib
+import pandas as pd
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
+import os.path as path
+
 
 # python -m nltk.downloader stopwords
+
+project_name = "Gifter.ai"
+net_id = "Shreya Subramanian: ss2745, Joy Zhang: jz442, Aparna Calambur: ac987, Ashrita Raman: ar699, Jannie Li: jl2578"
+
+
+@irsystem.route('/', methods=['GET'])
+def search():
+    
+
+    cur_path = pathlib.Path(__file__).parent.absolute().parent.absolute().parent.absolute()
+
+    #print(cur_path)
+    path_1 =  path.abspath(path.join(cur_path,"datafiles/reviews-m03.csv"))
+    print(path_1)
+    reviews_dct = create_review_list(path_1)
+    query = "cutterpede mom dinosaur moist damp lmao lamp pole"
+    print(boolean_search(reviews_dct, query))
+
+    query = request.args.get('search')
+    price = request.args.get('price')
+    if not price:
+        price = 50
+    if not query:
+        data = []
+        output_message = ''
+        asin_list = []
+    else:
+        output_message = "Relevant products"
+        asin_list = boolean_search(reviews_dct, query)
+        data = create_product_list(asin_list, float(price))
+    return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, asins=asin_list)
 
 def tokenize(text): 
     s = set(stopwords.words('english'))
@@ -15,19 +54,27 @@ def tokenize(text):
     filtered = [w for w in lower if not w in s] 
     return filtered
 
+def create_product_list(asin_list, price):
+    cur_path = pathlib.Path(__file__).parent.absolute().parent.absolute().parent.absolute()
+    path1 =  path.abspath(path.join(cur_path,"datafiles/metadata-m03.csv"))
+    metadata_df = pd.read_csv(path1)
+    product_list = []
+    for asin in asin_list:
+        row = metadata_df.loc[metadata_df.asin == asin]
+        title = str(row['title'].iloc[0])
+        out_price = '$' + str(row['price'].iloc[0])
+        if (float(row['price'].iloc[0]) <= price):
+            product_list.append(title + "   --   " + str(out_price))
+    return product_list
+
 def create_review_list(csv_name):
     result = dict()
     i = 0
-    with open(csv_name) as csvfile:
-        reviewsCSV = csv.reader(csvfile, delimiter=',')
-        for row in reviewsCSV:
-            if i == 0:
-                i += 1
-                continue
-            review_text = row[2] + " " + row[3]
-            tokenized = tokenize(review_text)
-            if (row[4] == "False"):
-                continue
+    reviews_df = pd.read_csv(csv_name)
+    reviews_df['summary_and_review'] = reviews_df['reviewText'] + reviews_df['summary']
+    for index, row in reviews_df.iterrows():
+        if(bool(row['verified'])):
+            tokenized = tokenize(row['summary_and_review'])
             if (row[0] in result):
                 result[row[0]] = result[row[0]] + tokenized
             else:
@@ -69,7 +116,7 @@ def filter_good_types(reviews_dct, good_types):
         reviews_dct[product] = good
     return reviews_dct
 
-reviews_dct = create_review_list("reviews-m03.csv")
+
 # good_types = find_good_types(reviews_dct) 
 # reviews_dct = filter_good_types(reviews_dct, good_types)
 
@@ -84,10 +131,4 @@ def boolean_search(reviews_dct, query):
         words = review_s.intersection(query_s)
         if (len(words) > 0):
             result.append(product)
-    return result
-
-
-
-query = "cutterpede mom dinosaur moist damp lmao lamp pole"
-
-print(boolean_search(reviews_dct, query))
+    return result[:4]
