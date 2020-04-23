d6a168edd472793268e4c250a093fe3ab43ecbed
Shreya Subramanian <ss2745@cornell.edu>
2020-04-23 19:27:49 -0400

    Added scoring system to rank the results on relevance

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,27 +1,34 @@
+from nltk.stem import PorterStemmer
+import pickle
+import os.path as path
+from nltk.tokenize import word_tokenize
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-import csv 
+import csv
 import re
-import nltk 
+import nltk
 from nltk.corpus import stopwords
 from pathlib import Path
 import pathlib
 import pandas as pd
 nltk.download('stopwords')
 nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-import os.path as path
-from nltk.stem import PorterStemmer 
-import pickle
 
 project_name = "Gifter.ai"
 net_id = "Shreya Subramanian: ss2745, Joy Zhang: jz442, Aparna Calambur: ac987, Ashrita Raman: ar699, Jannie Li: jl2578"
 
+TITLE_WEIGHT = 30
+REVIEW_WEIGHT = 1
+NUM_RESULTS = 10
+
 # Load inverted indices
-cur_path = pathlib.Path(__file__).parent.absolute().parent.absolute().parent.absolute()
-path_review_index =  path.abspath(path.join(cur_path,"datafiles/review_index.pickle"))
-path_title_index =  path.abspath(path.join(cur_path,"datafiles/title_index.pickle"))
+cur_path = pathlib.Path(__file__).parent.absolute(
+).parent.absolute().parent.absolute()
+path_review_index = path.abspath(
+    path.join(cur_path, "datafiles/review_index.pickle"))
+path_title_index = path.abspath(
+    path.join(cur_path, "datafiles/title_index.pickle"))
 
 with open(path_review_index, 'rb') as handle:
     review_index = pickle.load(handle)
@@ -29,8 +36,9 @@ with open(path_review_index, 'rb') as handle:
 with open(path_title_index, 'rb') as handle:
     title_index = pickle.load(handle)
 
+
 @irsystem.route('/', methods=['GET'])
-def search():    
+def search():
     query = request.args.get('search')
     price = request.args.get('price')
     if not price:
@@ -45,16 +53,20 @@ def search():
         data = create_product_list(asin_list, float(price))
     return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, asins=asin_list)
 
-def tokenize_query(text): 
+
+def tokenize_query(text):
     s = set(stopwords.words('english'))
     ps = PorterStemmer()
     words = re.findall("[a-zA-Z]+", text)
     filtered = [ps.stem(w).lower() for w in words if not w in s]
     return filtered
 
+
 def create_product_list(asin_list, price):
-    cur_path = pathlib.Path(__file__).parent.absolute().parent.absolute().parent.absolute()
-    path1 =  path.abspath(path.join(cur_path,"datafiles/merged-metadata-final.csv"))
+    cur_path = pathlib.Path(__file__).parent.absolute(
+    ).parent.absolute().parent.absolute()
+    path1 = path.abspath(
+        path.join(cur_path, "datafiles/merged-metadata-final.csv"))
     metadata_df = pd.read_csv(path1)
     product_list = []
     for asin in asin_list:
@@ -66,21 +78,65 @@ def create_product_list(asin_list, price):
                 product_list.append(title + "   --   " + str(out_price))
     return product_list
 
-#TODO use root of word
-def boolean_search(query):
-    query_tok = tokenize_query(query)
-    result = list()
-    for token in query_tok:
-        token_list = []
-        if(token in review_index):
-            review_asins = list(set([token_list.append(asin) for (_,asin,_) in review_index[token]]))
+
+def add_review_score(reviewerID, asin, count, review_score):
+    if asin in review_score:
+        if reviewerID in review_score[asin]:
+            review_score[asin][reviewerID] += REVIEW_WEIGHT * count
         else:
-            review_asins = []
+            review_score[asin][reviewerID] = REVIEW_WEIGHT * count
+    else:
+        review_score[asin] = {reviewerID: REVIEW_WEIGHT * count}
+
+
+def add_product_score(asin, score, product_score, review_score, title_score):
+    if(asin in product_score):
+        product_score[asin] += score
+    else:
+        product_score[asin] = score
+        review_score[asin] = {}
+        title_score[asin] = 0
+
+
+def boolean_search(query):
+    # to find the most relevant product
+    product_score = {}
+    # to find the most relevant reviews for a product
+    review_score = {}  # {asin -> {reviewid ->  score}}
+    # score how many query words the title of the product has
+    title_score = {}
+
+    query_tok = tokenize_query(query)
+    for token in query_tok:
+
+        if(token in review_index):
+            for (reviewerID, asin, count) in review_index[token]:
+                add_review_score(reviewerID, asin, count,
+                                 review_score)
+
         if(token in title_index):
             title_asins = list(set(title_index[token]))
+            for asin in title_asins:
+                if(asin in title_score):
+                    title_score[asin] += TITLE_WEIGHT
+                else:
+                    title_score[asin] = TITLE_WEIGHT
+
+    for asin in title_score.keys():
+        product_score[asin] = title_score[asin]
+    for asin in review_score.keys():
+        reviews = review_score[asin]
+        total_review_score = 0
+        for review in reviews.keys():
+            total_review_score += reviews[review]
+        total_review_score /= (len(reviews) + 1)
+
+        if asin in product_score:
+            product_score[asin] += total_review_score
         else:
-            title_asins = []
-        review_asins = list(set(review_asins).difference(set(title_asins)))
-        result += title_asins
-        result += review_asins
-    return result[:5]
+            product_score[asin] = total_review_score
+
+    product_score_sorted = {k: v for k, v in sorted(
+        product_score.items(), key=lambda item: item[1], reverse=True)}
+
+    return list(product_score_sorted.keys())[: NUM_RESULTS]
